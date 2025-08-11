2a51c37b38ec67fc664b1aaa9ae75e87967078f5
Soumyajit Tarafdar <soumyajit0212@gmail.com>
2025-08-11 02:59:42 -0400

    main.tsx original

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,22 @@
 /// <reference types="vinxi/types/client" />
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc, makeTRPCClient } from "./trpc/client";
 
 import "./styles.css";
+
 import { createRouter } from "./router";
 
+// Set up a Router instance
 const router = createRouter();
-const queryClient = new QueryClient();
-const trpcClient = makeTRPCClient();
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-          <RouterProvider router={router} />
-        </trpc.Provider>
-      </QueryClientProvider>
+      <RouterProvider router={router} />
     </React.StrictMode>,
   );
 }
