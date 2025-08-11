3107fa39b1bbcc83df088271198290b124be2d76
Soumyajit Tarafdar <soumyajit0212@gmail.com>
2025-08-11 02:38:05 -0400

    build: use Vinxi vercel preset. app.config changes for API trpc

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -8,7 +8,7 @@ export function createClient(getToken?: () => string | null) {
   return trpc.createClient({
     links: [
       httpBatchLink({
-        url: "/trpc",                    // ⬅️ must match app.config.ts base
+        url: "/api/trpc",                    // ⬅️ must match app.config.ts base
         headers() {
           const token = getToken?.();
           return token ? { authorization: `Bearer ${token}` } : {};
