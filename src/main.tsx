1b8866c3ae74a01aefc3fac6bbda811c6e90f1fa
Soumyajit Tarafdar <soumyajit0212@gmail.com>
2025-08-11 02:50:51 -0400

    build: use Vinxi vercel preset. app.config changes for API trpc, main

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,27 @@
 /// <reference types="vinxi/types/client" />
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc, makeTRPCClient } from "./trpc/client";
 
 import "./styles.css";
-
 import { createRouter } from "./router";
 
-// Set up a Router instance
 const router = createRouter();
+const queryClient = new QueryClient();
+const trpcClient = makeTRPCClient();
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <RouterProvider router={router} />
+        </trpc.Provider>
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }
