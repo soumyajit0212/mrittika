09e28ef1cfdd0ecd4c37e334f62b3b84b7e99742
Soumyajit Tarafdar <soumyajit0212@gmail.com>
2025-08-12 01:09:00 -0400

    fix(trpc): point client to /api/trpc

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -37,11 +37,11 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
           condition: (op) => op.type === "subscription",
           false: httpBatchStreamLink({
             transformer: SuperJSON,
-            url: getBaseUrl() + "/trpc",
+            url: getBaseUrl() + "/api/trpc",
           }),
           true: httpSubscriptionLink({
             transformer: SuperJSON,
-            url: getBaseUrl() + "/trpc",
+            url: getBaseUrl() + "/api/trpc",
           }),
         }),
       ],
